1f978a84040e611972dc6959cc68ca4b965147cf
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-21 19:03:04 -0300

    Add environment variable for development mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import { config } from 'dotenv';
 import StrangerThings from './components/StrangerThings';
+
+config();
+
+const { REACT_APP_IS_PRODUCTION } = process.env;
 
 function App() {
   return (
     <div className="App">
+      {REACT_APP_IS_PRODUCTION === 'true' && <h1>Em desenvolvimento</h1>}
       <StrangerThings />
     </div>
   );
